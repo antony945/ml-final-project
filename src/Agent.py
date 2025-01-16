e3d857475892b8566fc598e100fd8e68e86f284f
Antonio Franzoso <antonio.franzoso@outlook.it>
2025-01-16 17:11:48 +0100

    Code update

--- src/Agent.py
+++ src/Agent.py
@@ -12,7 +12,8 @@ class Agent:
         epsilon_decay: float,
         final_epsilon: float,
         discount_factor: float = 0.95,
-        load_from_file: bool = False
+        load_from_file: bool = False,
+        use_dict: bool = False
     ):
         # Initialize agent
         self.env = env
@@ -23,21 +24,42 @@ class Agent:
         self.discount_factor = discount_factor
         _, self.info = env.reset()
         self.training_error = []
+        self.use_dict = use_dict
 
+        # If obs space is continuous we must discretize it
+        self.obs_spaces_size = -1
+        self.obs_spaces = []
+        self.divisions = 10
+        if (not isinstance(self.env.observation_space, gym.spaces.Discrete)):
+            # print(self.env.observation_space.shape)
+            for i in range(0, self.env.observation_space.shape[0]):
+                # When an obs will be discretized it could have values between 0 and self.divisions included (self.divisions+1 total values)
+                space = np.linspace(env.observation_space.low[i], env.observation_space.high[i], self.divisions)
+                self.obs_spaces.append(space)
+                
+            # self.divisions+1 * obs_space.shape spaces
+            # q_table will have obs_spaces_size x action_spaces
+            self.obs_spaces_size = np.prod([len(s) + 1 for s in self.obs_spaces])
+        else:
+            self.obs_spaces_size = self.env.observation_space.n
+        
         # Create filename for the q-table based on the env
-        self.filename = f"{self.env.spec.name}"
+        self.envname = f"{self.env.spec.name}"
         for k,v in self.env.spec.kwargs.items():
             if k != "render_mode":
-                self.filename += f"_{v}"
-        self.filename += ".pk1"
+                self.envname += f"_{v}"
+        self.filename = f"{self.envname}.pk1"
 
         # Create q_table or load it from a file if specified
         if load_from_file:
             with open(self.filename, "rb") as f:
                 self.q_table = pickle.load(f)
         else:
-            # self.q_table = defaultdict(lambda: np.zeros(env.action_space.n))
-            self.q_table = defaultdict(self.create_numpy_entry) # can't use lambda due to pickle not able to "pickle" an anon function
+            if self.use_dict:
+                # self.q_table = defaultdict(lambda: np.zeros(env.action_space.n))
+                self.q_table = defaultdict(self.create_numpy_entry) # can't use lambda due to pickle not able to "pickle" an anon function
+            else:
+                self.q_table = np.zeros((self.obs_spaces_size, self.env.action_space.n))
 
     def create_numpy_entry(self):
             return np.zeros(self.env.action_space.n)
@@ -48,27 +70,55 @@ class Agent:
             return self.env.action_space.sample()
         else:
             # Exploitation -> Follow Q-table
-            return int(np.argmax(self.q_table[obs]))
+            obs = self.discretize_obs(obs)
+            if self.use_dict:
+                return int(np.argmax(self.q_table[obs]))
+            else:
+                return int(np.argmax(self.q_table[obs, :]))
+
+    def discretize_obs(self, obs):
+        # Don't discretize if obs is only one integer
+        if (isinstance(obs, int)):
+            return obs
+        # Index where to insert obs
+        obs_idx = sum([np.digitize(obs[i], self.obs_spaces[i]) * (len(self.obs_spaces[i]) ** i) for i in range(len(obs))])
+        return obs_idx
 
     def update(self,
-        obs: tuple[int, int, bool],
+        obs: tuple,
         action: int,
         reward: float,
         terminated: bool,
-        next_obs: tuple[int, int, bool],
+        next_obs: tuple,
         is_training=True
     ):
         if not is_training: return
 
+        obs = self.discretize_obs(obs)
+        next_obs = self.discretize_obs(next_obs)
+
+        # print(obs)
+        # print(next_obs)
+        # print(f"MAX SIZE: {self.obs_spaces_size}")
+        # input("...")
+
         # Update Q-table after having performed action
         # Q(s, a) = r + max Q(s', a')
-        future_q_value = (not terminated) * np.max(self.q_table[next_obs])
-        temporal_difference = reward + self.discount_factor * future_q_value - self.q_table[obs][action]
+        if self.use_dict:
+            future_q_value = (not terminated) * np.max(self.q_table[next_obs])
+            current_q_value = self.q_table[obs][action]
+            temporal_difference = reward + self.discount_factor * future_q_value - current_q_value
+            self.q_table[obs][action] += self.learning_rate * temporal_difference
+        else:
+            future_q_value = (not terminated) * np.max(self.q_table[next_obs, :])
+            current_q_value = self.q_table[obs, action]
+            temporal_difference = reward + self.discount_factor * future_q_value - current_q_value
+            self.q_table[obs, action] += self.learning_rate * temporal_difference
 
-        self.q_table[obs][action] += self.learning_rate * temporal_difference
         self.training_error.append(temporal_difference)
     
     def decay_epsilon(self):
+        # Linear decay
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
 
     def save_table(self):
--- src/main.py
+++ src/main.py
@@ -6,52 +6,8 @@ import logging
 from matplotlib import pyplot as plt
 import pickle
 
-def run(env_args: dict, n_episodes: int, is_training = False, show_plots = False, show_render = False):
-    # Initialise the environment
-    env = gym.make(**env_args, render_mode=None if not show_render else "human")
-    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
-
-    # hyperparameters
-    learning_rate = 0.01
-    start_epsilon = 1.0
-    epsilon_decay = start_epsilon / (n_episodes / 2) # reduce the exploration over time
-    final_epsilon = 0.1
-
-    agent = Agent(
-        env=env,
-        learning_rate=learning_rate,
-        initial_epsilon=start_epsilon,
-        epsilon_decay=epsilon_decay,
-        final_epsilon=final_epsilon,
-        load_from_file=not is_training
-    )
-    
-    for episode in tqdm(range(n_episodes)):
-        obs, info = env.reset()
-        done = False
-
-        # play one episode
-        while not done:
-            action = agent.get_action(obs, is_training=is_training)
-            # print(f"{episode} - Agent choose action: {action}")
-
-            next_obs, reward, terminated, truncated, info = env.step(action)
-
-            # update the agent
-            agent.update(obs, action, reward, terminated, next_obs, is_training=is_training)
-
-            # update if the environment is done and the current obs
-            done = terminated or truncated
-            obs = next_obs
-
-        agent.decay_epsilon()
-    
-    # Save on file
-    if is_training:
-        agent.save_table()
-
-    # Display plots
-    if show_plots:
+def plot_results(env, agent, mean_rewards, integrated = False):
+    if (integrated):
         # print(f'Episode time taken: {env.time_queue}')
         # print(f'Episode total rewards: {env.return_queue}')
         # print(f'Episode lengths: {env.length_queue}')
@@ -75,46 +31,163 @@ def run(env_args: dict, n_episodes: int, is_training = False, show_plots = False
             axs[2].set_title("Training Error")
             axs[2].set_xlabel("Episode")
             axs[2].set_ylabel("Temporal Difference")
+    else:
+        plt.plot(mean_rewards)
+        
+    plt.savefig(f"results/{agent.envname}_results_{len(mean_rewards)}.png")
+    plt.tight_layout()
+    plt.show()
 
-        plt.tight_layout()
-        plt.show()
+def test(env_args: dict, show_render = True):
+    # Initialise the environment
+    env = gym.make(**env_args, render_mode=None if not show_render else "human")
+    
+    print(f"Action space: {env.action_space}")
+    print(f"Obs space: {env.observation_space}")
 
+    obs, info = env.reset()
+    done = False
+
+    # play one episode
+    i = 0
+    while not done:
+        action = env.action_space.sample()
+        # print(f"{episode} - {i}) Agent choose action: {action}")
+
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+
+        i = i+1
+
+    return
+
+def run(env_args: dict, n_episodes: int, learning_rate: float, is_training = False, show_plots = False, show_render = False, debug = False, record_video = False):
+    # Initialise the environment
+    env = gym.make(**env_args, render_mode="rgb_array" if not show_render else "human") # or "none"
+
+    training_period = 5000
+    if is_training and record_video:
+        env = gym.wrappers.RecordVideo(env, video_folder="videos", name_prefix="training", episode_trigger=lambda x: x % training_period == 0)
+    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+    # hyperparameters
+    start_epsilon = 1.0
+    epsilon_decay = start_epsilon / (n_episodes/2) # reduce the exploration over time
+    # epsilon_decay = 5e-4
+    final_epsilon = 0.05
+
+    agent = Agent(
+        env=env,
+        learning_rate=learning_rate,
+        initial_epsilon=start_epsilon,
+        epsilon_decay=epsilon_decay,
+        final_epsilon=final_epsilon,
+        load_from_file=not is_training,
+        use_dict=True
+    )
+    
+    rewards_per_episode = []
+    mean_rewards = []
+    best_reward = -99999
+
+    iterable = range(n_episodes)
+    if not debug:
+        iterable = tqdm(iterable)
+
+    try:
+        for episode in iterable:
+        # for episode in range(n_episodes):
+            obs, info = env.reset()
+            done = False
+
+            rewards = 0
+
+            # play one episode
+            while not done:
+                action = agent.get_action(obs, is_training=is_training)
+                # print(f"{episode} - Agent choose action: {action}")
+
+                next_obs, reward, terminated, truncated, info = env.step(action)
+
+                # update the agent
+                agent.update(obs, action, reward, terminated, next_obs, is_training=is_training)
+
+                # update if the environment is done and the current obs
+                done = terminated or truncated
+                # done = terminated or rewards < -1000
+                obs = next_obs
+                rewards += reward
+            
+            if rewards > best_reward:
+                best_reward = rewards
+
+            rewards_per_episode.append(rewards)
+            mean_reward = np.mean(rewards_per_episode[len(rewards_per_episode)-100:])
+            mean_rewards.append(mean_reward)
+
+            if debug and is_training and episode%100==0:
+                print(f"Episode: {episode}, Epsilon: {agent.epsilon:0.2f}, Reward: {rewards:.3f}, Best Reward: {best_reward:.3f}, Mean Reward {mean_reward:0.3f}")
+
+            agent.decay_epsilon()
+        
+    except KeyboardInterrupt:
+        print("\nCTRL+C pressed.\n")
+
+    # Save on file
+    if is_training:
+        agent.save_table()
+
+    # Display plots
+    if show_plots:
+        plot_results(env, agent, mean_rewards, integrated=False)
+
+    env.close()
     return agent
 
 def main():
-    env_args = {
-        "id": "FrozenLake-v1",
-        "map_name": "8x8",
-        "is_slippery": True
-    }
+    # env_args = {
+    #     "id": "FrozenLake-v1",
+    #     "map_name": "8x8",
+    #     "is_slippery": True
+    # }
 
     # env_args = {
     #     "id": "Taxi-v3",
     # }
 
-    # env_args = {
-    #     "id": "LunarLander-v3",
-    #     "continuous": False,
-    #     "gravity": -10.0,
-    #     "enable_wind": False,
-    #     "wind_power": 15.0,
-    #     "turbulence_power": 1.5
-    # }
-
     env_args = {
-        "id": 'Blackjack-v1',
-        "natural": False,
-        "sab": False
+        "id": "LunarLander-v3",
+        "continuous": False,
     }
 
+    # env_args = {
+    #     "id": 'Blackjack-v1',
+    #     "natural": False,
+    #     "sab": False
+    # }
+
+    # env_args = {
+    #     "id": "MountainCar-v0",
+    # }
+
+    # TEST
+    # test(env_args, n_episodes)
+
     # Initialise the environment
-    n_episodes = 100_000
+    n_episodes = 50_000
+    learning_rate = 0.001
 
     # TRAINING
-    run(env_args, n_episodes, is_training=True, show_plots=True, show_render=False)
+    run(env_args, n_episodes, learning_rate, is_training=True, show_plots=True, show_render=False, debug=True, record_video=False)
 
     # RUN
-    run(env_args, 1000, is_training=False, show_plots=True, show_render=False)
+    run(env_args, 1000, learning_rate, is_training=False, show_plots=True, show_render=False, debug=False, record_video=False)
+
+    # RENDER
+    run(env_args, 5, learning_rate, is_training=False, show_plots=True, show_render=True, debug=False, record_video=False)
 
 if __name__ == '__main__':
     main()
